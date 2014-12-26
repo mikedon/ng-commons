4677f7449cea97fd44951c300e52c9c8ad93540e
mikedon <mikedonovan1987@gmail.com>
2014-12-26 00:40:25 -0500

    refactoring common service layer

--- src/ng-commons.js
+++ src/ng-commons.js
@@ -1,5 +1,116 @@
-angular.module('ng-commons', ['ui.bootstrap', 'ng-commons.BaseResource', 'ng-commons.User', 'ng-commons-tpls', 'ng-commons.input', 'ng-commons.navbar', 'ng-commons.navbarLink', 'ng-commons.loading']);
+angular.module('ng-commons', [
+		'ui.bootstrap', 
+		'ng-commons.resources', 
+		'ng-commons.BaseResource', 
+		'ng-commons.User', 
+		'ng-commons-tpls', 
+		'ng-commons.input', 
+		'ng-commons.navbar', 
+		'ng-commons.navbarLink', 
+		'ng-commons.loading'
+]);
 
+/*
+ * http://www.objectpartners.com/2014/06/03/extending-angulars-resource-service-for-a-consistent-api/
+ */
+angular.module('ng-commons.resources', [])
+.provider('api', function(){
+	return {
+		$get: ['$resource', function($resource){
+			var api = {
+      			defaultConfig : {id: '@id'},
+      			add : function (config) {
+        			var params, url;
+        			// If the add() function is called with a
+        			// String, create the default configuration.
+        			if (angular.isString(config)) {
+          				var configObj = {
+            				resource: config,
+            				url: '/api/' + config
+          				};
+          				config = configObj;
+        			}
+        			// If the url follows the expected pattern, we can set cool defaults
+        			if (!config.unnatural) {
+          				var orig = angular.copy(api.defaultConfig);
+          				params = angular.extend(orig, config.params);
+          				url = config.url + '/:id';
+        			// otherwise we have to declare the entire configuration. 
+        			} else {
+          				params = config.params;
+          				url = config.url;
+        			}
+        			// If we supply a method configuration, use that instead of the default extra. 
+        			var methods = config.methods || api.extraMethods;
+        			api[config.resource] = $resource(url, params, methods);
+      			}
+    		};    
+    		return api;
+		}]
+	};
+})
+.provider('data', function(){
+	return {
+    	$get: ['$rootScope', 'api', function ($rootScope, api) {
+    		var addAlerts = function(alerts){
+        		if(!alerts || alerts.length === 0){
+            		return;
+        		}
+        		if(!$rootScope.alerts){
+            		$rootScope.alerts = [];
+        		}
+        		$rootScope.alerts = $rootScope.alerts.concat(alerts);
+    		};
+			var beforeCall = function(){
+				$rootScope.loadingView = true;
+			};
+			var afterCall = function(){
+				$rootScope.loadingView = false;
+			};
+			var wrapPromise = function(promise) {
+				promise.then(function(data){
+					afterCall();
+					addAlerts(data.alerts);
+				}, function(data){
+					afterCall();
+					addAlerts(data.alerts);
+				});
+				return promise;
+			};
+      		var data = {
+        		query: function (resource, query) {
+					beforeCall();
+          			var promise = api[resource].query(query).$promise;  
+					return wrapPromise(promise);
+        		},
+        		get : function (resource, query) {
+					beforeCall();
+            		var promise = api[resource].get(query).$promise;
+					return wrapPromise(promise);
+        		},
+        		save : function (resource, model) {
+					beforeCall();
+          			var promise = api[resource].save(model).$promise;
+					return wrapPromise(promise);
+        		}, 
+        		update : function (resource, model) {
+					beforeCall();
+          			var promise = api[resource].update(model).$promise;
+					return wrapPromise(promise);
+        		},
+        		remove : function (resource, model) {
+					beforeCall();
+          			var promise = data.remove(resource, model).$promise;
+					return wrapPromise(promise);
+        		}
+      		};
+      		return data;
+    	}]
+	};
+});
+
+
+//TODO - refactor User service to not use BaseResource
 angular.module('ng-commons.BaseResource',[]).provider('BaseResource', function(){
 	return {
 		$get: ['$resource', '$http', '$rootScope', function($resource, $http, $rootScope){
